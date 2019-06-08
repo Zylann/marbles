8fbe2380952418a1c529cd579d8ae4327893be1a
Marc Gilleron <marc.gilleron@gmail.com>
2019-06-08 02:37:25 +0200

    Make it so sheeps look forward instead of rolling over

--- marble.gd
+++ marble.gd
@@ -1,9 +1,12 @@
 extends RigidBody
 
-const AVG_AMOUNT = 32
+const AVG_AMOUNT = 10
+
+onready var _visual = get_node("Node/Scene Root")
 
 var _ground_normal = Vector3.UP
 var _avg_ground_normal = Vector3.UP
+var _avg_pos = Vector3()
 
 
 func _ready():
@@ -12,6 +15,10 @@ func _ready():
 
 func get_avg_ground_normal():
 	return _avg_ground_normal
+
+
+func get_avg_position():
+	return _avg_pos
 
 
 func _process(delta):
@@ -34,5 +41,12 @@ func _integrate_forces(state):
 
 	var a = float(AVG_AMOUNT)
 	_avg_ground_normal = (_avg_ground_normal * (a - 1) + _ground_normal) / a
+	
+	_avg_pos = (_avg_pos * (a - 1.0) + translation) / a
+	var forward = (translation - _avg_pos).normalized()
 
-
+	_visual.translation = translation
+	var tmp_scale = _visual.scale
+	_visual.look_at(_visual.translation + forward, get_avg_ground_normal())
+	# TODO look_at resets scale in Godot 3.1, was fixed in master
+	_visual.scale = tmp_scale
