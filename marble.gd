fc047b997d409eda118df2573615f93196bcb0a5
Marc Gilleron <marc.gilleron@gmail.com>
2019-06-08 02:45:43 +0200

    Make sheep wiggle-run when they are in contact with ground

--- marble.gd
+++ marble.gd
@@ -7,6 +7,7 @@ onready var _visual = get_node("Node/Scene Root")
 var _ground_normal = Vector3.UP
 var _avg_ground_normal = Vector3.UP
 var _avg_pos = Vector3()
+var _last_contact_time = 0.0
 
 
 func _ready():
@@ -28,6 +29,8 @@ func _process(delta):
 
 
 func _integrate_forces(state):
+	var now = OS.get_ticks_msec()
+	
 	if state.get_contact_count() > 0:
 		var hit_pos = Vector3()
 		for i in state.get_contact_count():
@@ -36,6 +39,7 @@ func _integrate_forces(state):
 		var diff = state.transform.origin - hit_pos
 		if diff != Vector3():
 			_ground_normal = diff.normalized()
+		_last_contact_time = now
 	else:
 		_ground_normal = Vector3.UP
 
@@ -47,6 +51,12 @@ func _integrate_forces(state):
 
 	_visual.translation = translation
 	var tmp_scale = _visual.scale
-	_visual.look_at(_visual.translation + forward, get_avg_ground_normal())
+	var up = get_avg_ground_normal()
+	
+	var off = Vector3()
+	if now - _last_contact_time < 500.0:
+		off = 0.5 * up * sin(OS.get_ticks_msec() / 70.0)
+	
+	_visual.look_at(_visual.translation + forward + off, up)
 	# TODO look_at resets scale in Godot 3.1, was fixed in master
 	_visual.scale = tmp_scale
